ddd34d0d564c2baa785c59a0d429a6b5fafbc14b
ragvaldus <90042874+ragvaldus@users.noreply.github.com>
2023-02-21 11:15:59 +0100

    Added simulated training data

--- Code/Peter/init_modules.py
+++ Code/Peter/init_modules.py
@@ -12,12 +12,18 @@ from clustar.core import ClustarData
 from astropy.visualization import ZScaleInterval
 from astropy.io import fits
 from sklearn.model_selection import train_test_split
-from tensorflow.keras.datasets import mnist
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D
-from tensorflow.keras.layers import MaxPool2D
-from tensorflow.keras.layers import Flatten
-from tensorflow.keras.layers import Dropout
-from tensorflow.keras.layers import Dense
 import random
 from scipy.ndimage import rotate
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pprint
+
+import joblib
+import skimage
+from skimage.io import imread
+from skimage.transform import resize
+
+pp = pprint.PrettyPrinter(indent=4)
