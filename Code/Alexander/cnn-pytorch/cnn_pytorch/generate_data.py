ff4a61ca3b1ce17874e32a0bccc9a5debdcec766
enabty <ybring@student.chalmers.se>
2023-02-22 11:38:54 +0100

    Bugfix

--- Code/Alexander/cnn-pytorch/cnn_pytorch/generate_data.py
+++ Code/Alexander/cnn-pytorch/cnn_pytorch/generate_data.py
@@ -6,8 +6,8 @@ from PIL import Image
 # import numpy
 
 # Define the input and output directories
-input_dir = '../data/sets/input/'
-output_dir = '../data/sets/output/'
+input_dir = '../data/sets/input/'  # Which images to use for augmentation
+output_dir = '../data/sets/output/'  # Where to save the augmentated images
 
 # Define the list of transformations to be applied on images
 transformations = transforms.Compose([
@@ -24,12 +24,13 @@ for filename in os.listdir(input_dir):
     if filename.endswith('.png'):
         # Load the image from disk and convert to RGB mode
         img_path = os.path.join(input_dir, filename)
-        img = Image.open(img_path).convert('RGB')
+        img = Image.open(img_path).convert('RGB')  # This line was needed because somehow the inputs had 28 channels??
         
         # Apply the transformations to the image
         img_transformed = transformations(img)
         
         # Save the transformed image to the output directory
-        output_filename = os.path.splitext(filename)[0] + '_transformed.png'
+        new_str = '_transformed.png'
+        output_filename = os.path.splitext(filename)[0] + new_str
         output_path = os.path.join(output_dir, output_filename)
         torchvision.utils.save_image(img_transformed, output_path)
