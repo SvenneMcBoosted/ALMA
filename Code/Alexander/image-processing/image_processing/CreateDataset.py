3fca16547d4704e36ad80840fbe0a3f67a6ad7e7
enabty <ybring@student.chalmers.se>
2023-02-25 00:24:06 +0100

    feat: Add image-processing module

---
+++ Code/Alexander/image-processing/image_processing/CreateDataset.py
@@ -0,0 +1,95 @@
+import pandas as pd
+import csv
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+from astropy.nddata import Cutout2D
+from astropy import units
+from astropy.io import fits
+from astropy.modeling.models import Gaussian2D
+import glob
+from astropy.visualization import astropy_mpl_style
+from astropy.io import fits
+from astropy.utils.data import get_pkg_data_filename
+from clustar.core import ClustarData
+from astropy.visualization import ZScaleInterval
+from astropy.io import fits
+import random
+from scipy.ndimage import rotate
+
+def fits2pngFile(file_path, save_path, name):
+    data = fits.getdata(file_path)
+    zscale = ZScaleInterval()
+    data = zscale(data).squeeze()
+    plt.imshow(data)
+    plt.axis('off')
+    plt.ioff()
+    plt.savefig('./data/train/' + name + '.png', bbox_inches='tight', pad_inches=0)
+    plt.close()
+
+def fits2pngData(data, save_path, name):
+    zscale = ZScaleInterval(contrast=0.25, nsamples=1)
+    plt.figure(figsize=(im_size, im_size), dpi=100)
+    plt.imshow(zscale(data.data).squeeze(), origin='lower', cmap='rainbow', aspect='auto')
+    plt.axis('off')
+    plt.ioff()
+    plt.savefig('./data/train/' + name + '.png', bbox_inches='tight', pad_inches=0)
+    plt.close()
+
+train_positives = {os.path.splitext(f)[0] : 1 for f in os.listdir('./data/train/positives')}
+train_negatives = {os.path.splitext(f)[0] : 0 for f in os.listdir('./data/train/negatives')}
+eval_positives = {os.path.splitext(f)[0] : 1 for f in os.listdir('./data/eval/positives')}
+eval_negatives = {os.path.splitext(f)[0] : 0 for f in os.listdir('./data/eval/negatives')}
+train_dt = {**train_positives, **train_negatives}
+eval_dt = {**eval_positives, **eval_negatives}
+
+#augmentera här, lägg till .fits kopior i train_dt
+
+im_size = 100
+
+def find_object_pos(file):
+    cd = ClustarData(path=file, group_factor=0, threshold=0.025, metric="standard_deviation")
+    if len(cd.groups) > 0:
+        disk = cd.groups[0]
+        bounds = disk.image.bounds
+        x = (bounds[2] + bounds[3])/2 
+        y = (bounds[0] + bounds[1])/2
+        return (x, y)
+    else:
+        print("No object found in {}".format(file))
+        return None
+
+
+#Spara alla .fits filer som png för inmatninig i CNN
+for name, label in train_dt.items():
+    if (label):
+        img_data = fits.getdata('./data/train/positives/' + name + '.fits')
+        object_pos = find_object_pos('./data/train/positives/' + name + '.fits')
+    else:
+        img_data = fits.getdata('./data/train/negatives/' + name + '.fits')
+        object_pos = find_object_pos('./data/train/negatives/' + name + '.fits')
+    
+    if object_pos != None:
+        # Data shape is (1, 1, x, y) we want it to be (x, y)
+        img_data.shape = (img_data.shape[2], img_data.shape[3])
+        # Set the size of the crop in pixels
+        crop_size = units.Quantity((im_size, im_size), units.pixel)
+        data = Cutout2D(img_data, object_pos, crop_size)
+    if (label): fits2pngData(data, './data/train/', name)
+    else: fits2pngData(data, './data/train/', name)
+    
+
+for name, label in eval_dt.items():
+    if (label): fits2pngFile('./data/eval/positives/' + name + '.fits', './data/eval/', name)
+    else: fits2pngFile('./data/eval/negatives/' + name + '.fits', './data/eval/', name)
+
+#Spara annotations för false och positives
+with open('./data/train/annotations.csv', 'w') as f:
+    for key in train_dt.keys():
+        f.write("%s %s\n"%(key,train_dt[key]))
+
+with open('./data/eval/annotations.csv', 'w') as f:
+    for key in eval_dt.keys():
+        f.write("%s %s\n"%(key,train_dt[key]))
+
+
