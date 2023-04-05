5afa5fe7c4ac1f345f3d34029efc883d62315380
RasmussenElias <raelias@chalmers.se>
2023-04-05 15:19:11 +0200

    new code for combo every image

--- Code/Eilas/augment.py
+++ Code/Eilas/augment.py
@@ -64,6 +64,55 @@ def save_geo_mean(fits_data1, fits_data2, im_size, png_folder):
     im_name2 = fits_data2.strip('.fits')[6:]
     save_png(im_geo, im_name1+im_name2, png_folder)
 
+# Test 
+def fits2tensor(fits_folder, im_size):
+    filenames = glob.glob(fits_folder + '/*.fits')
+    tensor1 = []
+    for file in filenames:
+        data = fits2standard(file, im_size)
+        tensor1.append(data)
+    return tensor1
+
+
+# Takes every posible geometric mean combination of a tensor, tensor-input
+def geo_mean_comb(tensor):
+    combi = []
+    name = 0
+    for i in range(len(tensor)):
+        for j in range(i+1,len(tensor)):
+            combi.append([i,j])
+    geo_mean_tensor = []
+    for i in range(len(combi)):
+        data1 = tensor[combi[i][0]]
+        data2 = tensor[combi[i][1]]
+        geo = im_geo_mean(data1,data2)
+        geo_mean_tensor.append(geo)
+        plt.imshow(geo.squeeze(), origin='lower', aspect='auto')
+        plt.axis('off')
+        plt.imsave('shit2' + '/'+ 'pos_test' + str(name) + '.png',geo.squeeze(), cmap='CMRmap_r')
+        name += 1
+    return geo_mean_tensor
+
+fits_folder = 'data3'
+rotation_matrix = [0, 0, -9, -9, -13, 0]
+im_size = 100
+
+fil = og2standard(fits_folder, rotation_matrix, im_size)
+fil2 = geo_mean_comb(fil)
+
+fil1 = glob.glob('data2/*.fits')[0]
+fi2 = glob.glob('data2/*.fits')[1]
+data1 = fits2standard(fil1,100).squeeze()
+data2 = fits2standard(fil2,100).squeeze()
+
+geo = im_geo_mean(data1,data2)
+plt.imshow(geo, cmap='CMRmap_r')
+plt.show()
+
+
+
+
+
 
 #### INTE KLARA GREJER ####
 # Moves the object randomly, image-input : Not done
